85709fc25fa581e4cf48c13b589fd2da6d6b5b38
FARO_Z <zhangjia_good@126.com>
2021-07-16 14:55:57 +0800

    将user-service 添加到 eureka 注册中心

--- user-service/src/main/java/top/faroz/UserServiceApplication.java
+++ user-service/src/main/java/top/faroz/UserServiceApplication.java
@@ -3,7 +3,9 @@ package top.faroz;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient // 开启 eureka 客户端
 @SpringBootApplication
 @MapperScan("top.faroz.mapper")
 public class UserServiceApplication {
