77f10dbed08bef9f9c766f14d5ef56742653ea11
FARO_Z <zhangjia_good@126.com>
2021-07-16 15:44:59 +0800

    添加消费者配置（接口调用后面使用 feign ，这里先不配置了）

--- consumer/src/main/java/top/faroz/ConsumerApplication.java
+++ consumer/src/main/java/top/faroz/ConsumerApplication.java
@@ -2,8 +2,10 @@ package top.faroz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient // 开启 eureka 注册
 public class ConsumerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConsumerApplication.class, args);
